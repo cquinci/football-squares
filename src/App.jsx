7f8d1d8e9358fc8f6ce188bbfb7d47a03762f89c
cquinci <caq015@gmail.com>
2026-01-14 18:30:51 -0500

    Hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useCallback, useEffect, Fragment, useRef } fr
 
 // --- CONFIGURATION --- //
 // UPDATED API URL:
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxOBAMVDt-rUrp_g8clpMjySDBtGwwdcll6dG3q_lx0cpEuqstcAo9UnD3dAmCUREUyiw/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzcbv-FcA0viWI6RJesOrOCfJCUjnb5NmuMhzDnsSLA6ggdR66ODNdDgn2-sHu0fMOwrA/exec';
 
 // --- HELPER ICONS --- //
 // Adjusted icons to use currentColor for better theme compatibility
